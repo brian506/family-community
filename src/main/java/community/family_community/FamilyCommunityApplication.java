082492f24666e67e5902ee443590ec12a01a251c
brian <brian506@naver.com>
2025-09-30 23:02:49 +0900

    first commit

---
+++ src/main/java/community/family_community/FamilyCommunityApplication.java
@@ -0,0 +1,13 @@
+package community.family_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyCommunityApplication.class, args);
+	}
+
+}
